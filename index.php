4b1c728bf89b0055454322dd488ddb33d316bbe5
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-04-26 07:17:55 +0600

    updated

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Hello from the virtual host";
+$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+
+$pdo = new PDO($dsn, "root","",[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$stmt = $pdo->query("SELECT * FROM product");
+
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);
